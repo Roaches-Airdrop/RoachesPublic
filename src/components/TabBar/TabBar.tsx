3664e32610337e939203166d97f6c2a336cfedd5
bobbytech1 <aiyevbekpendivine@gmail.com>
2024-11-21 11:24:36 +0100

    Fixed the tab bar style

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -19,31 +19,31 @@ const TabBar = () => {
         <div className="fixed bottom-0 w-full z-[1000]">
             <div className="flex items-center justify-between bg-white bg-opacity-10 backdrop-blur-md rounded-tr-[50px] rounded-tl-[50px] py-4 px-6 shadow-lg">
             <div onClick={() => setSelectedTab("Earn")} className={`flex flex-col  py-[6px] px-6  justify-center items-center cursor-pointer ${
-                selectedTab === "Earn" ? "bg-[#ffffff25] py-[6px] px-6 shadow-lg rounded-[10px]" : ""
+                selectedTab === "Earn" ? "bg-[#ffffff25] py-[6px] px-4 shadow-lg rounded-[10px]" : ""
             }`}>
             <img src={Coin} alt="coin" className="h-6"/>
                 <p className={`text-center font-roachfont1 text-white text-[12px]`}>Earn</p>
             </div>
             <div onClick={() => setSelectedTab("Mine")} className={`flex flex-col  py-[6px] px-6  justify-center items-center cursor-pointer ${
-                selectedTab === "Mine" ? "bg-[#ffffff25] py-[6px] px-6 shadow-lg rounded-[10px]" : ""
+                selectedTab === "Mine" ? "bg-[#ffffff25] py-[6px] px-4 shadow-lg rounded-[10px]" : ""
             }`}>
             <GiMining  className="text-white text-[22px]"/>
                 <p className={`text-center font-roachfont1 text-white text-[12px]`}>Mine</p>
             </div>
             <div onClick={() => setSelectedTab("FrensRoaches")} className={`flex  py-[6px] px-6  flex-col justify-center items-center cursor-pointer ${
-                 selectedTab === "FrensRoaches" ? "bg-[#ffffff25] py-[6px] px-6 shadow-lg rounded-[10px]" : ""
+                 selectedTab === "FrensRoaches" ? "bg-[#ffffff25] py-[6px] px-4 shadow-lg rounded-[10px]" : ""
             }`}>
             <SiCockroachlabs  className="text-white text-[22px]"/>
                     <p className={`text-center font-roachfont1 text-white text-[12px]`}>Roaches</p>
             </div>
             <div onClick={() => setSelectedTab("Ranks")} className={`flex  py-[6px] px-6  flex-col justify-center items-center cursor-pointer ${
-                 selectedTab === "Ranks" ? "bg-[#ffffff25] py-[6px] px-6 shadow-lg rounded-[10px]" : ""
+                 selectedTab === "Ranks" ? "bg-[#ffffff25] py-[6px] px-4 shadow-lg rounded-[10px]" : ""
             }`}>
             <MdOutlineLeaderboard  className="text-white text-[22px]"/>
                     <p className={`text-center font-roachfont1 text-white text-[12px]`}>Ranks</p>
             </div>
             <div onClick={() => setSelectedTab("Upgrade")} className={`flex flex-col  py-[6px] px-6  justify-center items-center cursor-pointer ${
-                 selectedTab === "Upgrade" ? "bg-[#ffffff25] py-[6px] px-6 shadow-lg rounded-[10px]" : ""
+                 selectedTab === "Upgrade" ? "bg-[#ffffff25] py-[6px] px-4 shadow-lg rounded-[10px]" : ""
             }`}>
             <GrUpgrade className="text-white text-[22px]"/>
                     <p className={`text-center font-roachfont1 text-white text-[12px]`}>Upgrade</p>
