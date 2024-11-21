ec1743eb7103a9163a61f961d201f214800d1233
bobbytech1 <aiyevbekpendivine@gmail.com>
2024-11-21 13:16:14 +0100

    Fixed the tab style

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -15,7 +15,7 @@ const TabBar = () => {
         <>
         <div className="flex items-center justify-center">
             <div className="fixed bottom-0 w-full z-[1000]">
-                <div className="flex flex-wrap items-center justify-between bg-white bg-opacity-10 backdrop-blur-md rounded-tr-[20px] rounded-tl-[20px] py-4 xsm:px-6 px-2 shadow-lg">
+                <div className="flex flex-wrap items-center justify-between bg-white bg-opacity-10 backdrop-blur-md rounded-tr-[40px] rounded-tl-[40px] py-4 xsm:px-6 px-4 shadow-lg">
                     {[
                         { label: "Earn", icon: <img src={Coin} alt="coin" className="h-4" /> },
                         { label: "Mine", icon: <GiMining className="text-white text-[20px]" /> },
@@ -25,7 +25,7 @@ const TabBar = () => {
                         <div 
                             key={label}
                             onClick={() => setSelectedTab(label)} 
-                            className={`flex flex-col items-center cursor-pointer py-[6px] px-2 transition-all duration-100 ${
+                            className={`flex flex-col items-center cursor-pointer py-[6px] px-5 transition-all duration-100 ${
                                 selectedTab === label ? "bg-[#ffffff25] shadow-lg rounded-[10px]" : ""
                             }`}>
                             {icon}
